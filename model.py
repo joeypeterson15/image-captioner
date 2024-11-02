c1d9449f11fe903296e3e50ea4681917df6a0d06
Joey Peterson <joey.peterson15@gmail.com>
2024-11-02 09:00:03 -0700

    first push

---
+++ model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size, train_CNN=False):
+        super(EncoderCNN, self).__init__()
+        self.train_CNN = train_CNN
+        self.inception = models.inception_v3(pretrained=True, aux_logits=False)
+        self.inception.fc = nn.Linear(self.inception.fc.in_features, embed_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout()
+
+    def forward(self, images):
+        features = self.inception(images)
+
+        for name, param in self.inception.named_parameters():
+            if "fc.weight" in name or "fc.bias" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = self.train_CNN
